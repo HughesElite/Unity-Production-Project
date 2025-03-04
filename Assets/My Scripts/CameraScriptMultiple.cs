e6ca1b6a42b26eb964e4768a737b463f3dfa39f2
Ashley H <100367853+HughesElite@users.noreply.github.com>
2025-03-04 19:58:37 +0000

    Created script that allows switching between multiple cameras in a scene

---
+++ Assets/My Scripts/CameraScriptMultiple.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera camera1;
+    public Camera camera2;
+
+    void Start()
+    {
+        // Ensure only camera1 is active at the start
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+
+    void Update()
+    {
+        // Press 'C' to toggle cameras
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            camera1.enabled = !camera1.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+    }
+}
